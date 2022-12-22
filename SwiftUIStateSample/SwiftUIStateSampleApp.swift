4b4a13f37d76aad01315f8737b847e17493a76f9
Juan <projectnoa@users.noreply.github.com>
2022-12-21 20:31:37 -0700

    Initial Commit

---
+++ SwiftUIStateSample/SwiftUIStateSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//	SwiftUIStateSampleApp.swift
+//  SwiftUIStateSample
+//
+//  Created by Juan Mueller on 12/21/22.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
